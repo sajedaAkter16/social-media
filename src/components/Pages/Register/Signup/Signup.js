238afae3924a2d5e3ea1d5b24f71728cd25462af
Sajeda Akter <sajeda16akter@gmail.com>
2023-03-29 00:04:51 +0600

    'navbar-complete'

--- src/components/Pages/Register/Signup/Signup.js
+++ src/components/Pages/Register/Signup/Signup.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 className="text-5xl font-bold">Create a new account!</h1>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
@@ -22,8 +22,8 @@ const Signup = () => {
                 </label>
                 <input type="text" placeholder="password" className="input input-bordered" />
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
+              <Link>Forget password</Link>
+                  </label>
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
